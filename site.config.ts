889791a8786d181ae9ee9d1b0a8d4559dd08e247
f-moonrise <1332691109@qq.com>
2023-06-05 18:25:33 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd963380de7fa4ab78ff22dba895a4f1e',
+  rootNotionPageId: 'Personal-Home-d963380de7fa4ab78ff22dba895a4f1e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
